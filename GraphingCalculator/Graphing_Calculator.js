05aadac8f41c50cfb5ff0fb15d4c52969c05443a
Mwayi Kashoko <39887498+Greeniscool1@users.noreply.github.com>
2021-10-20 20:45:04 -0400

    Update Graphing_Calculator.js

--- GraphingCalculator/Graphing_Calculator.js
+++ GraphingCalculator/Graphing_Calculator.js
@@ -1,36 +1,56 @@
-<html><head>
-        <title> Graphing Calculator </title>
-		
-		<style>
-			body {
-				font-family: Helvetica;
-			}
-		
-			#canvas {
-				border: solid 1px black;
-				height: 71vh;
-			}
+"use strict";
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
 
-			#graph {
-				border-radius: 6px;
-				background: linear-gradient(gray, white);
-			}
-		</style>
-    </head>
-    <body>
-        <canvas id="canvas" height="400" width="400"> </canvas> <br> <br>
-		
-		No need to enter y=mx+b, just enter it as mx+b. <br> <br>
-		
-		Also if you're using parentheses then add the opererator before the parentheses to avoid an error. EXAMPLES: x*(3+5), x/(2*4) <br> <br>
-		
-		Enter a function to graph: <input placeholder="Enter a function" type="text" id="function"> <br>
-		
-        <input type="button" value="Graph" id="graph" onclick="graph();"> <br>
-		
-		<a href="file:///C:/Users/John/Saved%20Games/Games.html"> Back to my website </a>
-		
-		<script src="jsFunctions.js"> </script>
-		<script src="Graphing_Calculator.js"> </script>
-    
-</body></html>
+const graphWidth = width/20;
+const graphHeight = height/20;
+
+ctx.scale(20, 20);
+
+var func = document.getElementById("function");
+
+function drawLines() {
+	for (let i = 0; i < graphWidth; i++) {
+		ctx.beginPath();
+		ctx.lineWidth = 1/20;
+		ctx.strokeStyle = "black";
+		ctx.moveTo(0, i);
+		ctx.lineTo(graphWidth, i);
+		ctx.moveTo(i, 0);
+		ctx.lineTo(i, graphHeight);
+		ctx.stroke();
+		ctx.closePath();
+	}
+	
+	ctx.beginPath();
+	ctx.strokeStyle = "black";
+	ctx.lineWidth = 2.5/20;
+	ctx.moveTo(0, graphHeight/2);
+	ctx.lineTo(graphWidth, graphHeight/2);
+	ctx.moveTo(graphWidth/2, 0);
+	ctx.lineTo(graphWidth/2, graphHeight);
+	ctx.stroke();
+	ctx.closePath();
+}
+
+drawLines();
+
+function graph() {
+	ctx.fillStyle = "blue";
+	ctx.clearRect(0, 0, graphWidth, graphHeight);
+	
+	drawLines();
+
+	ctx.save();
+	ctx.translate(graphWidth/2, graphHeight/2);
+	
+	for (let x = -20; x < graphHeight; x+=1/20) {
+		if (x > -10 && x < graphWidth) {
+			ctx.fillRect(x, eval(func.value) * -1, 2/20, 2/20);
+		}
+	}
+	
+	ctx.restore();
+}
